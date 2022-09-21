017447e5caa4fdc0792f7e58612f03c390d0ecd6
Severo <cmsdof@gmail.com>
2022-09-21 16:44:02 -0300

    Exercicio Algoritmo
    Atualizando arquivo Algoritmo.js de teste de push para resposta

--- Exercicios/Algoritmo.js
+++ Exercicios/Algoritmo.js
@@ -1,5 +1,87 @@
 /*
-    Exercício de Algoritmo
+    Exercício:
 
-    Teste
+    Descreva um algoritmo para representar sua rotina matinal, a partir do momento em que você acorda. Use ao menos duas vezes a expressão Se ... Então ....
+
+    Caso queira ir um pouco além do que foi apresentado no vídeo, tente incluir as instruções Se ... Então ... Senão ... e Enquanto ....
+
+    Não há resposta certa ou errada.
+
+*/
+
+/* 
+    Segue um Esboço da Rotina Matinal que acontece diariamente entre 5 e 6:30 da manhã.
+    Comecei com uma linguagem mais de alto nível, mas fui achando divertido brincar com estruturas mais de linguagens de programação e ficou esse mistureba.
+
+    abrir os olhos
+    levantar da cama
+    ir.banheiro
+
+    Se a caixadeAreia estiver cheia
+        limpe a caixadeAreia
+    fimse
+
+    escovar os dentes
+
+    ir.cozinha
+    pegar sacola
+
+    ir.sala
+    pegar coleira
+    diana.call
+    diana = diana + coleira
+    sair de casa
+
+    ir até a praça
+    soltar coleira
+
+    diana.brincar
+    t = 0
+    enquanto necessidades == falso || t <= 900
+        espere
+        t++
+
+    se nessecidades == verdadeiro
+        usar sacola
+        pegar necessidades
+    fimse
+    ir.casa
+
+    diana = diana - coleira
+    lavar as mãos
+    diana.alimentar
+
+    preparar café da manhã
+    esquentar agua
+    enquanto agua.temperatura < 100
+        se tem pão
+            fazer ovo
+            café da manhã = pão com ovo
+        senão se tiver tapioca
+            pegar ovo
+            fazer crepioca
+            café da manhã = crepioca
+        fimse
+        fimse
+
+    pegar coador e café
+    coador = coador + café
+    garrafa = garrafa + coador + agua
+
+    café da manhã = café da manhã + garrafa
+    café da manhã.comer
+
+    ir.quarto
+    chen.acordar
+    pegar roupa
+
+    ir.banheiro
+    tomar banho
+    trocar de roupa
+
+    ir.trabalho
+
+
+
+
 */
