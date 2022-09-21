88cadaed74ce6119eab500da24ec0db8886e8d16
Severo <cmsdof@gmail.com>
2022-09-21 19:07:29 -0300

    Exercicio M3A9
    Funçoes 1.0

---
+++ Exercicios/M3A9.js
@@ -0,0 +1,26 @@
+/* 
+    Exercícios:
+
+    1. Crie uma função chamada imprimir que recebe um argumento e simplesmente imprime ele na tela;
+    
+    2. Lembra dos exercícios que propusemos na Aula 05 - Variáveis? Com base neles, faça o seguinte:
+        Crie uma função para cada algoritmo proposto lá, exceto o número 6;
+        Todas as funções devem retornar o valor final;
+        Procure dar nomes que façam sentido às funções. Ex.: a função para o item 4 poderia se chamar mediaAritmetica ou calcularMediaAritmetica;
+        Procure utilizar comentários para documentar cada método;
+        Na função da média aritmética, evite colocar todos os itens como argumentos do método, pois isso limitaria sua utilização a um número fixo de elementos. Tente implementar uma lógica para receber um array e calcular a média em cima dele, tornando a função mais reaproveitável em diferentes situações.
+    
+    3. Com as funções criadas, execute todo código abaixo e verifique se os resultados são os esperados (o resultado esperado de cada função está no comentário ao lado da chamada):
+
+*/
+
+x = "Teste Positivo"
+
+function imprimir(x) {
+     console.log(x)
+  }
+ 
+imprimir(x)
+
+
+
