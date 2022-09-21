0f8b45b71b2ea6a8595c08898bfee6f01d5fc423
Severo <cmsdof@gmail.com>
2022-09-21 20:17:14 -0300

    Exercicio M3A9
    Completo

--- Exercicios/M3A9.js
+++ Exercicios/M3A9.js
@@ -14,6 +14,7 @@
 
 */
 
+console.log("Exercício 1:")
 x = "Teste Positivo"
 
 function imprimir(x) {
@@ -22,5 +23,90 @@ function imprimir(x) {
  
 imprimir(x)
 
+console.log("\n")
+console.log("\n")
 
+console.log("Exercício 2:")
+console.log("Teste Nomes Iguals")
+function mesmoNome(nome1,nome2) {
+    if (nome1 == nome2){
+        return "Os nomes são iguais."
+    
+    } else return ("Os nomes são diferentes")
+ }
+ console.log("Teste 1:", mesmoNome("x","x"), "-- Resultado Esperado: 'Os nomes são iguais.' ")
+ console.log("Teste 2:", mesmoNome("x","y"), "-- Resultado Esperado: 'Os nomes são diferentes.' ")
+ console.log("\n")
+
+ console.log("Teste Maioridade")
+ let idade
+ function maiorDeIdade(idade){
+    if (idade >= 18) {
+        return "Maior de Idade"
+    } else return "Menor de Idade"
+ }
+ console.log("Teste 1:", maiorDeIdade(17), "-- Resultado Esperado: 'Menor de Idade' ")
+ console.log("Teste 2:", maiorDeIdade(18), "-- Resultado Esperado: 'Maior de Idade' ")
+ console.log("Teste 3:", maiorDeIdade(19), "-- Resultado Esperado: 'Maior de Idade' ")
+ console.log("\n")
+
+ console.log("Teste Boleto com Juros")
+ let boleto
+ function valorComJuros(boleto){
+    return boleto + boleto*0.1
+ }
+ console.log("Teste 1:", valorComJuros(100), "-- Resultado Esperado: '110' ")
+ console.log("Teste 2:", valorComJuros(1000), "-- Resultado Esperado: '1100' ")
+ console.log("\n")
+
+ console.log("Teste Média Aritmética")
+ let item
+ function mediaAritmetica(item) {
+    soma = 0
+    for (i=0; i < item.length; i++){
+        soma = soma + item[i]
+    }
+    return soma/item.length
+ } 
+ console.log("Teste 1:", mediaAritmetica(1,2,3,4,5), "-- Resultado Esperado: '3' ")
+ console.log("Teste 2:", mediaAritmetica(5,5,5,5,5), "-- Resultado Esperado: '5' ")
+ console.log("\n")
+
+ console.log("Teste MargemBruta")
+ function margemBruta(receitaLiquida,custos){
+    lucroBruto = receitaLiquida - custos
+    return (lucroBruto/receitaLiquida)*100
+ }
+ console.log("Teste 1:", margemBruta(100,50), "-- Resultado Esperado: '50%' ")
+ console.log("Teste 2:", margemBruta(200,50), "-- Resultado Esperado: '75%' ")
+ console.log("\n")
+ console.log("\n")
+ 
+ console.log("Exercício 3:")
+ cwi = "CWI"
+ reset = "Reset"
+ imprimir(mesmoNome(cwi, cwi))   // true
+ imprimir(mesmoNome(cwi, reset)) // false
+ 
+ imprimir("---")
+ 
+ imprimir(maiorDeIdade(30)) // true
+ imprimir(maiorDeIdade(18)) // true
+ imprimir(maiorDeIdade(5))  // false
+ 
+ imprimir("---")
+ 
+ imprimir(valorComJuros(100))   // 110
+ imprimir(valorComJuros(984.5)) // 1082.95
+ 
+ imprimir("---")
+ 
+ imprimir(mediaAritmetica([1]))             // 1
+ imprimir(mediaAritmetica([1, 4, 10]))      // 5
+ imprimir(mediaAritmetica([1, 2, 3, 4, 5])) // 3
+ 
+ imprimir("---")
+ 
+ imprimir(margemBruta(1000000, 500000))      // 50
+ imprimir(margemBruta(528459.11, 632501.87)) // -19.68[...]
 
