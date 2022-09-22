49a1116154e62933fe86338dfff594d13d1e5992
Severo <cmsdof@gmail.com>
2022-09-22 17:53:25 -0300

    Adicionando Assert

--- cypress/cypress/e2e/todo.cy.js
+++ cypress/cypress/e2e/todo.cy.js
@@ -1,8 +1,24 @@
 describe('TodoMVC', () => {
 
     it('Adicionar uma nova tarefa', () => {
+        //Teste 1
         cy.visit("https://todomvc-app-for-testing.surge.sh/")
-        cy.get("input.new-todo")
+        cy.get("input.new-todo").type('Teste de Automação{enter}')
+
+        //Assert
+        cy.get('ul.todo-list li').should('have.length', 1)
+        //---
+
+         //Teste 2
+         cy.visit("https://todomvc-app-for-testing.surge.sh/")
+         cy.get("input.new-todo").type('Teste de Automação{enter}')
+         cy.get("input.new-todo").type('Tarefa 2{enter}')
+         cy.get("input.new-todo").type('Tarefa 3{enter}')
+         cy.get("input.new-todo").type('Tarefa 4{enter}')
+         cy.get("input.new-todo").type('Tarefa 5{enter}')
+         //Assert
+         cy.get('ul.todo-list li').should('have.length', 5)
+         //---
     })
 });
 
