fb22d584a289b82532c70141d664cb0118fb2c7f
Severo <cmsdof@gmail.com>
2022-11-08 14:29:32 -0300

    :tada: Commit Inicial Desafio de Automatização de Testes FrontEnd
    Criando ambiente

---
+++ desafio-autoweb/cypress/e2e/features/criarConta.feature
@@ -0,0 +1,6 @@
+Feature: Criar Conta
+
+    Scenario: Criar Conta
+        Given que eu acesso o site
+        When eu crio uma Conta
+        Then eu vejo a tela de 'My Account'
---
+++ desafio-autoweb/cypress/e2e/tests/testeQueFalha.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+import HomePage from "../../pages/home-page.js";
+
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+  })
+})
