3c6388e7a64201f29eb2eda7f36543b3786147f0
Severo <cmsdof@gmail.com>
2022-11-10 11:07:10 -0300

    Setup Feature - Cucumber

---
+++ desafio-autoweb/cypress/support/step_definitions/accountCreation.steps.js
@@ -0,0 +1,13 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
+
+Given("que eu acesso o site", () => {
+    return "true";
+    });
+
+When("eu crio uma Conta", () => {
+    return "true";
+    });
+
+Then("eu vejo a tela de 'My Account'", () => {
+    return "true";
+    });
