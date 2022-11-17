15d2e6c964b79c6212ee2f83fecaa900b04a9406
Severo <cmsdof@gmail.com>
2022-11-17 10:51:14 -0300

    Adicionando Testes de Contrato

--- testes-api/cypress/api/wooCommerce/productCategories.cy.js
+++ testes-api/cypress/api/wooCommerce/productCategories.cy.js
@@ -4,11 +4,17 @@ import productCategoriesSchema from '../../contratos/categories'
 
 
 describe ('Product Categories', () => {
-    it ('Listar todas as categorias', () => {
+    it ('Listar todas as categorias - Aceitação', () => {
         //Transformar CyRequest em commando
         cy.getProductCategoriesWooCommerce().should((listarCategoriasResponse) => {
             expect(listarCategoriasResponse.status).to.be.eq(200)
             expect(listarCategoriasResponse.body).to.have.length.greaterThan(0)
+        })
+    })
+
+    it('Lista todas as categorias - Contrato', () => {
+        cy.getProductCategoriesWooCommerce().should((listarCategoriasResponse) => {
+            productCategoriesSchema.validateAsync(listarCategoriasResponse.body)
         })
     })
 
--- testes-api/cypress/contratos/categories.js
+++ testes-api/cypress/contratos/categories.js
@@ -1,25 +1,24 @@
-/* import Joi from "joi";
+import Joi from "joi";
 
 
 const productCategoriesSchema = Joi.array().items(Joi.object({
-    id: Joi.number().required(),
-    name: Joi.string().required(),
-    slug: Joi.string().required(),
-    parent: Joi.number().required(),
-    description: Joi.string().allow('').required(),
-    display: Joi.number().required(),
-    image: Joi.string().allow(null).required(),
-    menu_order: Joi.number().required(),
-    count: Joi.number().required(),
-    _links: Joi.object({
-        self: Joi.array.items(Joi.object({
-            href: Joi.string().required()
+    "id": Joi.number().required(),
+    "name": Joi.string().required(),
+    "slug": Joi.string().required(),
+    "parent": Joi.number().required(),
+    "description": Joi.string().allow('').required(),
+    "display": Joi.number().required(),
+    "image": Joi.string().allow(null).required(),
+    "menu_order": Joi.number().required(),
+    "count": Joi.number().required(),
+    "_links": Joi.object({
+        "self": Joi.array().items(Joi.object({
+            "href": Joi.string().required()
         })),
-        colletion: Joi.array().items(Joi.object({
-            href: Joi.string().required()
+        "colletion": Joi.array().items(Joi.object({
+            "href": Joi.string().required()
         }))
-    }),
-    "": Joi.number().required()
+    })
 }))
 
-export default productCategoriesSchema */
+export default productCategoriesSchema
