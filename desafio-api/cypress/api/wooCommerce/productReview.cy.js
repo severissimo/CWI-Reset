cddb5f547ae8ce007012c101fc52d7a4b79bec41
Severo <cmsdof@gmail.com>
2022-11-20 17:19:37 -0300

    :heavy_check_mark: Product Reviews: Passando Teste - DELETE Aceitação

--- desafio-api/cypress/api/wooCommerce/productReview.cy.js
+++ desafio-api/cypress/api/wooCommerce/productReview.cy.js
@@ -8,23 +8,52 @@ import { faker } from '@faker-js/faker'
 import { exist } from 'joi'
 
 describe('API - Product Reviews', () => {
-  it.only('GET: Listar todos os Product Reviews - Aceitação', () => {
+  const newReview = faker.datatype.uuid()
+
+  it('GET: Listar todos os Product Reviews - Aceitação', () => {
     cy.getAllProductReviewsWooCommerce(token.token)
-    .should((response) => {
-      expect(response.status).equals(statusFixture.ok)
-      expect(response.body).exist
-    })  
+      .should((response) => {
+        expect(response.status).equals(statusFixture.ok)
+        expect(response.body).exist
+      })
   })
-  
+
   it('POST: Criar um Product Review - Aceitação', () => {
-    cy.visit('https://example.cypress.io')
+    cy.postProductReviewsWooCommerce(
+      token.token,
+      productReviewFixture.product_id,
+      newReview,
+      productReviewFixture.reviewer,
+      productReviewFixture.reviewer_email,
+      productReviewFixture.rating
+    ).then((response) => {
+      cy.deleteProductReviewsWooCommerce(token.token, response.body.id)
+    })
   })
-  
+
   it('PUT: Update um Product Review - Aceitação', () => {
     cy.visit('https://example.cypress.io')
   })
-  
-  it('DELETE: Apagar um Product Review - Aceitação', () => {
-    cy.visit('https://example.cypress.io')
-  })  
+
+  it.only('DELETE: Apagar um Product Review - Aceitação', () => {
+    cy.postProductReviewsWooCommerce(
+      token.token,
+      productReviewFixture.product_id,
+      newReview,
+      productReviewFixture.reviewer,
+      productReviewFixture.reviewer_email,
+      productReviewFixture.rating
+    ).then((response) => {
+        cy.deleteProductReviewsWooCommerce(token.token, response.body.id)
+          .should((response) => {
+            expect(response.status).equals(statusFixture.ok)
+            expect(response.body.deleted).equals(productReviewFixture.deleted)
+            expect(response.body.previous.product_id).equals(productReviewFixture.product_id)
+            expect(response.body.previous.review).equals(newReview)
+            expect(response.body.previous.reviewer).equals(productReviewFixture.reviewer)
+            expect(response.body.previous.reviewer_email).equals(productReviewFixture.reviewer_email)
+            expect(response.body.previous.rating).equals(productReviewFixture.rating)
+          })
+      })
+  })
 })
--- desafio-api/cypress/support/wooCommerce/productReview.commands.js
+++ desafio-api/cypress/support/wooCommerce/productReview.commands.js
@@ -5,33 +5,46 @@ import productReviewFixture from '../../fixtures/productReviewFixture.json'
 Cypress.Commands.add('getAllProductReviewsWooCommerce', (token) => {
     cy.request({
         method: 'GET',
-        url: Cypress.config('baseUrl') + productReviewFixture.url,            
+        url: Cypress.config('baseUrl') + productReviewFixture.url,
         headers: { Authorization: token }
     })
 })
 
-Cypress.Commands.add('postProductReviewsWooCommerce', (token) => {
+Cypress.Commands.add('postProductReviewsWooCommerce', (
+    token,
+    product_id,
+    review,
+    reviewer,
+    reviewer_email,
+    rating
+    ) => {
     cy.request({
-        method: '',
-        url: Cypress.config('baseUrl') + productReviewFixture.url,            
+        method: 'POST',
+        url: Cypress.config('baseUrl') + productReviewFixture.url,
         headers: { Authorization: token },
-        body: {}
+        body: {
+            product_id: product_id,
+            review: review,
+            reviewer: reviewer,
+            reviewer_email: reviewer_email,
+            rating: rating
+        }
     })
 })
 
 Cypress.Commands.add('putProductReviewsWooCommerce', (token) => {
     cy.request({
         method: '',
-        url: Cypress.config('baseUrl') + productReviewFixture.url,            
+        url: Cypress.config('baseUrl') + productReviewFixture.url,
         headers: { Authorization: token },
         body: {}
     })
 })
 
-Cypress.Commands.add('deleteProductReviewsWooCommerce', (token) => {
+Cypress.Commands.add('deleteProductReviewsWooCommerce', (token, id) => {
     cy.request({
-        method: '',
-        url: Cypress.config('baseUrl') + productReviewFixture.url,            
+        method: 'DELETE',
+        url: Cypress.config('baseUrl') + productReviewFixture.url + id + productReviewFixture.force,
         headers: { Authorization: token }
     })
 })
